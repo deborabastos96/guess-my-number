b12cbf0b5c713e946abed9f41979fc84d05a098f
Débora Bastos <deborabastos96@gmail.com>
2023-06-22 10:43:02 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Guess My Number
+
+This folder contains the archives from the lessons on creating the Guess My Number project.
+It is a simple game created using HTML, CSS, and JavaScript. The goal is to guess the number using as few guesses as possible. Have fun!
---
+++ script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = `🎉 Correct number!`;
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 18;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); */
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage(`⛔ No Number!`);
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage(`🎉 Correct number!`);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? `📈 Too high` : `📉 Too low`);
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    displayMessage(`💥 You lost the game!`);
+    document.querySelector('.score').textContent = 0;
+  }
+});
+
+// AGAIN BUTTON
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage(`Start guessing...`);
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = `?`;
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
